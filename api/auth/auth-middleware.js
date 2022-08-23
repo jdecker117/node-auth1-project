f7f01e0d9a98ee9c6e06f4d9fe894f89196c3ac7
Jason <jdecker117@gmail.com>
2022-08-23 10:50:23 -0400

    wip

--- api/auth/auth-middleware.js
+++ api/auth/auth-middleware.js
@@ -1,3 +1,4 @@
+const User = require('../users/users-model')
 /*
   If the user does not have a session saved in the server
 
@@ -18,8 +19,16 @@ next()
     "message": "Username taken"
   }
 */
-function checkUsernameFree(req, res, next) {
-next()
+async function checkUsernameFree(req, res, next) {
+  try{
+    const user = await User.findBy({username: req.body.username})
+    if(!user.length){
+      next()
+    }
+    else(next({"message": "Username taken"}))
+  } catch(err){
+    next(err)
+  }
 }
 
 /*
